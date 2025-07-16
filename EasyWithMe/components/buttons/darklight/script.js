9666b9f65cccf14a5919984b59b3c3f7125a0de3
Rehan <rehanmujawarofficial007@gmail.com>
2025-07-16 19:33:55 +0530

    Delete EasyWithMe directory

--- EasyWithMe/components/buttons/darklight/script.js
+++
@@ -1,5 +0,0 @@
-function toggleMode() {
-  document.body.classList.toggle("dark-mode");
-  const btn = document.querySelector(".btn-toggle-mode");
-  btn.textContent = document.body.classList.contains("dark-mode") ? "☀ Light Mode" : "🌙 Dark Mode";
-}
--- EasyWithMe/components/buttons/onoff/script.js
+++
@@ -1,4 +0,0 @@
-function toggleSwitch(btn) {
-  btn.classList.toggle("on");
-  btn.textContent = btn.classList.contains("on") ? "ON" : "OFF";
-}
